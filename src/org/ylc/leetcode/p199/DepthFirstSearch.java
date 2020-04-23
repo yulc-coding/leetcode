0df9ceacb118a5c6ecebdc099589900d35aefa9e
superylcfly <350009335@qq.com>
2020-04-23 17:02:30 +0800

    p199

--- src/org/ylc/leetcode/p199/DepthFirstSearch.java
+++ src/org/ylc/leetcode/p199/DepthFirstSearch.java
@@ -1,14 +1,16 @@
 package org.ylc.leetcode.p199;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 代码千万行，注释第一行，
  * 注释不规范，同事泪两行。
  * <p>
  * 199. 二叉树的右视图
- * 执行用时 : 3 ms, 在所有 Java 提交中击败了 8.10% 的用户
- * 内存消耗 : 38.6 MB, 在所有 Java 提交中击败了 5.00% 的用户
+ * 深度优先搜索，优先右节点
+ * 执行用时 : 1 ms, 在所有 Java 提交中击败了 97.36% 的用户
+ * 内存消耗 : 38.4 MB, 在所有 Java 提交中击败了 5.00% 的用户
  *
  * @author YuLc
  * @version 1.0.0
@@ -16,48 +18,31 @@ import java.util.*;
  */
 public class DepthFirstSearch {
 
+    List<Integer> result = new ArrayList<>();
+
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
-        // 存放每层对应的最右节点的值， k,v -> 层数，最右边的非null值
-        Map<Integer, Integer> rightSideMap = new HashMap<>();
-        // 最大深度
-        int maxDepth = 0;
-        // 记录几点
-        Stack<TreeNode> nodeStack = new Stack<>();
-        // 记录当前节点对应的深度
-        Stack<Integer> depthStack = new Stack<>();
-        // 初始化根节点
-        nodeStack.push(root);
-        depthStack.push(0);
-
-        TreeNode curNode;
-        int curDepth;
-        while (!nodeStack.isEmpty()) {
-            curNode = nodeStack.pop();
-            curDepth = depthStack.pop();
-            if (curNode != null) {
-                // 重置最大深度
-                maxDepth = Math.max(maxDepth, curDepth);
-                // 只记录第一次出现的值，即最右边的值
-                if (!rightSideMap.containsKey(curDepth)) {
-                    rightSideMap.put(curDepth, curNode.val);
-                }
-                // 后入先出，所以要先放left
-                nodeStack.push(curNode.left);
-                depthStack.push(curDepth + 1);
-
-                nodeStack.push(curNode.right);
-                depthStack.push(curDepth + 1);
-            }
-        }
-        // 按层数获取对应的值
-        for (int depth = 0; depth <= maxDepth; depth++) {
-            result.add(rightSideMap.get(depth));
+        if (root != null) {
+            // 根节点深度为0
+            dfs(root, 0);
         }
         return result;
     }
 
+    /**
+     * 按照 根节点 -> 右子节点 -> 左子节点的顺序遍历
+     */
+    private void dfs(TreeNode root, int depth) {
+        // 将当前层数的首个（即最右边的放入结果中）
+        if (result.size() == depth) {
+            result.add(root.val);
+        }
+        depth++;
+        // 优先访问右边子节点
+        if (root.right != null) {
+            dfs(root.right, depth);
+        }
+        if (root.left != null) {
+            dfs(root.left, depth);
+        }
+    }
 }
